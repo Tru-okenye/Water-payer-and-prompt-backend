fe85de43e3bd6c4858abc07ddc774db203b78ac5
Truphy <truphenaokenye@gmail.com>
2023-11-24 10:00:45 +0300

    ggf

--- PAYER/PAYER/settings.py
+++ PAYER/PAYER/settings.py
@@ -72,7 +72,6 @@ REST_FRAMEWORK = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
